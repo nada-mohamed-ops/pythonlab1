0c08bd11465448f52a8adb36a427c70b38ac1c5e
nada-mohamed-ops <nadaelshahd7000@gmail.com>
2024-08-10 20:46:51 +0300

    python lab1

---
+++ lab1.py
@@ -0,0 +1,9 @@
+# Q1
+amount = 47.28
+tip_persent = 0.15
+total_amount = (amount * tip_persent)+amount
+amount_per_person = total_amount / 2
+print(f'Each person needs to pay:{amount_per_person}')
+
+
+# Q2
